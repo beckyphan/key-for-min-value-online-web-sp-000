e6f368aba467ab79bf11caea7e273813e63778db
Becky Phan <beckyphan@live.com>
2019-05-31 01:08:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,6 +11,7 @@
 def key_for_min_value(name_hash)
   key_arr = []
   value_arr = []
+  min_key
   
   if name_hash.length == 0 
     return nil 
@@ -19,11 +20,12 @@ def key_for_min_value(name_hash)
       key_arr << key 
       value_arr << value 
     end 
-    
     counter = 0
-    
-    while counter < value_arr.length 
-      
+    if counter < value_arr.length 
+      if value_arr[counter] < value_arr[counter + 1]
+        min_key = key_arr[counter]
+      elsif value_arr[counter + 1] == error 
+        
     
     
 end
